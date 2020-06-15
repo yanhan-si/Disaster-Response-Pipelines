2cf04b3607185e32383d9072f0ad027e568c447d
yanhan-si <syanhan96@gmail.com>
2020-06-14 21:29:02 -0400

    Add documents

--- data/process_data.py
+++ data/process_data.py
@@ -3,36 +3,57 @@ import sys
 import pandas as pd
 from sqlalchemy import create_engine
 
+
 def load_data(messages_filepath="./data/disaster_messages.csv",
               categories_filepath="./data/disaster_categories.csv"):
-    messages = pd.read_csv(messages_filepath)
-    categories = pd.read_csv(categories_filepath)
-    return messages, categories
+    '''Load csv files into dataframes and return'''
+    messages_df = pd.read_csv(messages_filepath)
+    categories_df = pd.read_csv(categories_filepath)
+    return messages_df, categories_df
 
 
-def clean_data(messages, categories):
+def clean_data(messages_df, categories_df):
+    ''''
+    INPUT
+    messages_df - pandas dataframe
+    categories_df - pandas dataframe
+
+    OUTPUT
+    df - a cleaned and concatenated dataframes from the two inputs
+
+    This function cleans df using the following steps :
+    1. Inner join messages_df and categories_df on the id
+    2. Split categories into separate category columns
+    3. Rename the categories columns
+    4. One hot encoding the categories
+    5. Drop the origianl categories column from df
+    6. Concatenate messages_df and categories_df
+    7. Remove duplicates
+    '''
     # merge datasets
-    df = messages.merge(categories, how='inner', on=['id'])
+    df = messages_df.merge(categories_df, how='inner', on=['id'])
 
     # create a dataframe of the 36 individual category columns
-    categories = df.categories.str.split(pat=';', expand=True)
+    categories_df = df.categories.str.split(pat=';', expand=True)
 
     # use the first row to extract a list of new column names for categories
-    category_colnames = categories.iloc[0].apply(func=lambda x: x[:-2]).to_list()
-    categories.columns = category_colnames
+    category_colnames = categories_df.iloc[0].apply(
+        func=lambda x: x[:-2]
+    ).to_list()
+    categories_df.columns = category_colnames
 
-    for column in categories:
+    for column in categories_df:
         # set each value to be the last character of the string
-        categories[column] = categories[column].apply(lambda x: x[-1])
+        categories_df[column] = categories_df[column].apply(lambda x: x[-1])
 
         # convert column from string to numeric
-        categories[column] = categories[column].astype(int)
+        categories_df[column] = categories_df[column].astype(int)
 
     # drop the original categories column from `df`
     df.drop(columns=['categories'], inplace=True)
 
     # concatenate the original dataframe with the new `categories` dataframe
-    df = pd.concat([df, categories], axis=1)
+    df = pd.concat([df, categories_df], axis=1)
 
     # drop duplicates
     df.drop_duplicates(inplace=True)
@@ -40,28 +61,34 @@ def clean_data(messages, categories):
 
 
 def save_data(df, database_filepath):
+    '''Save data into database'''
     engine = create_engine('sqlite:///'+database_filepath)
     filename = database_filepath.split("/")
     df.to_sql(filename[1][:-3], engine, index=False)
 
 
 def main():
+    '''This main function performs the following steps :
+    1. Load in data from .csv files
+    2. Clean data
+    3. Save the cleaned data into database
+    '''
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
-        messages, categories = load_data(messages_filepath, categories_filepath)
+        messages_df, categories_df = load_data(messages_filepath, categories_filepath)
 
         print('Cleaning data...')
-        df = clean_data(messages, categories)
-        
+        df = clean_data(messages_df, categories_df)
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 from sqlalchemy import create_engine
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
@@ -10,9 +11,11 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 import pickle
+import re
 
 
 def load_data(database_filepath):
+    '''Load data into dataframe from the database'''
     engine = create_engine('sqlite:///./'+database_filepath)
     df = pd.read_sql_table('DisasterResponse', engine)
     X = df.message.values
@@ -22,18 +25,38 @@ def load_data(database_filepath):
 
 
 def tokenize(text):
+    '''
+    INPUT
+    text - string
+
+    OUTPUT
+    clean_tokens - a list of words
+
+    This function processes the input using the following steps :
+    1. Remove punctuation characters
+    2. Tokenize text into list
+    3. Lemmatize, Normalize and Strip each word
+    4. Remove stop words
+    '''
+    # Remove punctuation characters
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    # Tokenize text
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
     clean_tokens = []
     for tok in tokens:
+        # Reduce words to their root form and normalize
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
+        if clean_tok not in stopwords.words("english"):
+            # Remove stop words
+            clean_tokens.append(clean_tok)
 
     return clean_tokens
 
 
 def build_model():
+    '''Build a machine learning pipeline'''
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
@@ -41,27 +64,37 @@ def build_model():
     ])
     return pipeline
 
+
 def evaluate_model(model, X_test, Y_test, category_names):
     return model.score(X_test, Y_test)
 
 
 def save_model(model, model_filepath):
+    # Export your model as a pickle file
     pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
+    '''This main function performs the following steps :
+    1. Load in data from database
+    2. Split data into train and text sets
+    3. Build a model using machine learning pipeline
+    4. Fit the model using the train dataset
+    5. Evaluate on the test dataset
+    6. Save the model
+    '''
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
