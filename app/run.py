d2394bacd490276dce8402770464f76e8d23f529
yanhan-si <syanhan96@gmail.com>
2020-08-23 21:39:39 -0400

    Update run.py

--- app/run.py
+++ app/run.py
@@ -10,7 +10,7 @@ from nltk.corpus import stopwords
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-#from sklearn.externals import joblib
+from sklearn.externals import joblib
 from sqlalchemy import create_engine
 import joblib
 
